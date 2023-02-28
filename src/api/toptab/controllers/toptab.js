261a8190dca52fd31e4534691252d1dcbaaa6546
banxianjia <2594787016@qq.com>
2023-02-28 17:54:36 +0800

    完善toptabs表结构

---
+++ src/api/toptab/controllers/toptab.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * toptab controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::toptab.toptab');
---
+++ src/api/toptab/routes/toptab.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * toptab router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::toptab.toptab');
